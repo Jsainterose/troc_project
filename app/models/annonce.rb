126ab4a615b2fe10a4adda4fd483b1d9fb25e47d
Jsainterose <sainterosejeremy@gmail.com>
2019-02-28 11:20:39 +0100

    annonce_pageV1

--- app/models/annonce.rb
+++ app/models/annonce.rb
@@ -1,7 +1,7 @@
 class Annonce < ApplicationRecord
     belongs_to :user
     belongs_to :category
-    paginates_per 6
+    paginates_per 3
 
     has_attached_file :image, styles: { medium: "600x600>", thumb: "300x300>" }, default_url: "/images/:style/missing.png"
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
