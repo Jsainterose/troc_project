991bf6686859377958d64b16ab8a9b65270fdcbf
Jsainterose <sainterosejeremy@gmail.com>
2019-03-18 13:36:10 +0100

    backofficeV1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :set_admin
+
   protect_from_forgery with: :exception
+
+  private
+  def set_admin
+    @admin = User.find_by(email: "admin@admin.com")
+  end
 end
